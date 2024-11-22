f77e9b607c55a0a52eaa6464162ad146de1000a4
LDegraz <degrazial1@yahoo.com>
2024-11-21 21:27:24 -0500

    added clear storage & reload page on clearBtn

---
+++ script.js
@@ -0,0 +1,18 @@
+const noteBoard = document.querySelector('#note-board');
+const addTextBtn = document.querySelector('#add-note-btn');
+const addListBtn = document.querySelector('#add-list-btn');
+const clearBtn = document.querySelector('#clear-all-btn');
+
+let tempStorageNote = {
+  text: [],
+  lists: [],
+};
+
+//Clears local storage & reloads page when clear button is clicked
+clearBtn.addEventListener('click', function () {
+  localStorage.clear();
+  location.reload();
+});
+
+
+
