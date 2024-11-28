5982a15a127706d13d7925e689624a2eac82590d
Tobi <tkorlewala@gmail.com>
2024-11-27 20:51:58 -0500

    can add remove and move note

--- scripts.js
+++ scripts.js
@@ -1,25 +1,129 @@
 let noteId=1;
 
 //start
-function createNotePad(){
-    const addNoteButton = document.getElementById('add-note-button');
+let newX = 0, newY = 0;
+let startX = 0;
+let startY = 0;
 
-    const notePad = document.getElementById('div');
-    notePad.className='note-pad';
-    notePad.id='note'+noteId;
+//set the current note to null
+let currentNote = null;
+function createNote(){
+    const notePad = document.getElementById('notes-board');
+    //create a note 
+    const note = document.createElement('div');
+    note.className='note';
+    note.id='note'+noteId;
 
-    const clearallButton = document.getElementById('span');
-    clearAllButton.className = 'clear-all-button';
-    clearAllButton.innerHTML = '&times;';
-    clearAllButton.onclick = function(){
-        noteContainer.removechild(notePad);
+
+    //Testing for the abilty to move notes
+    note.style.position = 'absolute';//can be deleted after setting poistion in styles to aboulte
+    note.style.display = 'block';//can be deleted
+
+    //clear button
+    const clearButton = document.createElement('span');
+    clearButton.className = 'clear-button';
+    clearButton.innerHTML = '&times;';
+    clearButton.onclick = function(){
+        notePad.removeChild(note);
         saveNotes();
     }
+
+    //moveable button
+    const MoveableButton = document.createElement('span');
+    MoveableButton.className = 'moveable-button';
+    MoveableButton.innerHTML = '+';
+    MoveableButton.onclick = function(){
+        if(note.classList.contains('moveable')){
+            note.classList.remove('moveable');
+        }
+        else{
+            note.classList.add('moveable');
+        }
+    }
     
+    //set the note content
     const noteContent = document.createElement('div');
     noteContent.contentEditable = true;
     noteContent.className = 'note-content';
     noteContent.innerHTML = 'Click to Edit';
 
-    noteContent.addEventListener
+    //edit content on the note
+    noteContent.addEventListener('click',function(){
+        noteContent.innerHTML='';
+    });
+
+    //Add clear, moveable, content to the note
+    note.appendChild(clearButton);
+    note.appendChild(MoveableButton);
+    note.appendChild(noteContent);
+    //add note to notePad
+    notePad.appendChild(note);
+
+    //[WIP] to set the color of the note
+    applyColor(note);
+
+    //add event lisiner to note
+    note.addEventListener('mousedown',mouseDown);
+
+    //every time a new note is added increase id by 1
+    noteId += 1;
+}
+
+function mouseDown(event){
+    //set the staring x and y to the cursor x and y
+    startX = event.clientX;
+    startY = event.clientY;
+
+    //set what is being clicked to clickedElement
+    let clickedElement = event.target;
+
+    //Check if the element that is being clicked on is has the class note
+    if(clickedElement.className == 'note'){
+        currentNote = clickedElement;
+    }
+
+    // For Debugging
+    //console.log('Mouse is down', startX, startY,clickedElement, currentNote)
+    if(currentNote.classList.contains('moveable')){
+        document.addEventListener('mousemove', mouseMove)
+        document.addEventListener('mouseup',mouseUp);
+    }
+}
+
+function mouseMove(event) {
+    newX = startX - event.clientX;
+    newY = startY - event.clientY;
+
+    startX = event.clientX;
+    startY = event.clientY;
+
+    currentNote.style.top = (currentNote.offsetTop - newY) + 'px';
+    currentNote.style.left = (currentNote.offsetLeft - newX) + 'px';
+
+    console.log('Mouse is down', currentNote.style.top, currentNote.style.left)
+
+
+}
+
+function mouseUp(event){
+    document.removeEventListener('mousemove',mouseMove);
+}
+
+function applyColor(note){
+    const color = '#FFCC00';
+    note.style.backgroundColor = color;
+}
+
+function saveNotes(){
+    const notes=document.querySelectorAll('.note');
+    const savedNotes=[];
+
+    notes.forEach((note)=>{
+        savedNotes.push({
+            id:note.id,
+            content:note.querySelector('.note-content').innerHTML,
+        });
+    });
+    localStorage.setItem('notes',JSON.stringify(savedNotes));
+
 }
